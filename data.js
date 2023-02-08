d28eecc6143b98a6c2744ce138ac384c91bb0ac9
vm-cheolubak <dev.cheolubak@valueupsys.com>
2023-02-09 08:49:35 +0900

    add last wrap

--- data.js
+++ data.js
@@ -11,7 +11,8 @@ function ${fileName}({ ...props }: ${fileName}Props) {
   return <${fileName}Styled>${fileName}</${fileName}Styled>;
 }
 
-export default ${fileName};`,
+export default ${fileName};
+`,
 
   styledData: (fileName) => `import styled from '@emotion/styled';
 import tw from 'twin.macro';
@@ -21,22 +22,26 @@ import tw from 'twin.macro';
  **/
 export const ${fileName}Styled = styled.div([
   tw\`\`
-]);`,
+]);
+`,
 
   propsData: (fileName) => `import { HTMLAttributes } from 'react';
 
 export interface ${fileName}Props extends HTMLAttributes<HTMLElement> {
 }`,
 
-  indexData: (fileName) => `export { default } from './${fileName}';`,
+  indexData: (fileName) => `export { default } from './${fileName}';
+`,
 
   storeData: (fileName) => `import { atom } from 'recoil';
 
 export const ${fileName}State = atom({
   key: \`${fileName}State\`,
-  default: null
-})`,
+  default: null,
+});
+`,
 
   modelData: (fileName) => `export interface ${fileName}Model {
-}`,
+}
+`,
 };
